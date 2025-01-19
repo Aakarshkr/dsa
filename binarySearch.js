22643a71cd20d61d36092ac3a594ec597006b70e
Aakarshkr <akarshkr88@gmail.com>
2025-01-19 11:30:57 +0530

    learning binary search

---
+++ binarySearch.js
@@ -0,0 +1,11 @@
+
+//only work at sorted array
+const binarySearch = (arr,t)=>{
+let lefti= 0;
+let righti=arr.length-1
+while(lefti<=righti){
+    
+}
+}
+
+console.log(binarySearch([-5,2,4,7,10,40], 10));
