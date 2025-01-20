f84f73281b9b086e035a0f2b987460cd5e845273
Aakarshkr <akarshkr88@gmail.com>
2025-01-20 19:34:42 +0530

    bubble sort

--- binarySearch.js
+++ binarySearch.js
@@ -1,11 +1,21 @@
+// Only works on sorted arrays
+const binarySearch = (arr, t) => {
+    let lefti = 0;
+    let righti = arr.length - 1;
 
-//only work at sorted array
-const binarySearch = (arr,t)=>{
-let lefti= 0;
-let righti=arr.length-1
-while(lefti<=righti){
-    
-}
-}
+    while (lefti <= righti) {
+        let mid = Math.floor((lefti + righti) / 2);
 
-console.log(binarySearch([-5,2,4,7,10,40], 10));
+        if (arr[mid] === t) {
+            return mid; 
+        } else if (arr[mid] < t) {
+            lefti = mid + 1; // Search in the right half
+        } else {
+            righti = mid - 1; // Search in the left half
+        }
+    }
+
+    return 'Not Found'; 
+};
+
+console.log(binarySearch([-5, 2, 4, 7, 10, 40], 10)); // Output: 4
