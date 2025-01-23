1d2f49e7d44ee5e69ddd95ce35e4d6b6b5d0449c
Aakarshkr <akarshkr88@gmail.com>
2025-01-23 20:29:31 +0530

    quicksort done uisng recursion mehtod

--- quickSort.js
+++ quickSort.js
@@ -1,4 +1,18 @@
 const quickSort =(arr)=>{
+    if (arr.length<2) {
+        return arr
+    }
+    let pivot = arr[0];
+    let left = []
+    let right = []
+    for (let i = arr.length-1; i > 0; i--) {
+        if (arr[i] < pivot) {
+            left.push(arr[i])
+        }else{
+            right.push(arr[i])
+        }
+        
+    }return [...quickSort(left) , pivot ,...quickSort(right)]
 
 }
 
